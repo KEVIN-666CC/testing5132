5abfc43a1e4c57c8e002bcc2700d5fea0ab1ed81
KohZhen <kohzhen0102@gmail.com>
2021-05-07 21:37:26 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace homework_1st
         static void Main(string[] args)
         {
             Console.WriteLine("你好！我是姚德勇");//update
+            Console.WriteLine("Test Kz here");//update
             Console.ReadLine();//hello world
         }
     }
