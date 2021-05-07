ea972144f4d63459ed3f864a39d5ef8b23dd7397
KEVIN-666CC <79522516+KEVIN-666CC@users.noreply.github.com>
2021-05-07 19:54:12 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace homework_1st
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("你好！我是姚德勇");
+            Console.ReadLine();
+        }
+    }
+}
